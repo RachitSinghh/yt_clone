e2ec5604c0d280a602c2a2b1c469568829d925bf
Rachit <rachitsingh72@gmail.com>
2026-04-09 03:02:01 +0530

    feat: implement user registration route and controller, update asyncHandler

--- src/app.js
+++ src/app.js
@@ -11,7 +11,13 @@ app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //CORS_ORIGIN
 app.use(express.static("public")); // for public assets
-app.use(cookieParser())
+app.use(cookieParser());
 
+// routes
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
